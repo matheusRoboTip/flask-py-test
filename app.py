38fef9181052c48234fa6454ce6b7ef01c7ca23a
matheusRoboTip <matheus.carvalho@robotip.com.br>
2024-02-20 12:26:01 -0300

    add-cors

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/simulate')
 def hello_world():
-  try:
-    return 'Hello, World!'
-  except Exception as e:
-    print(f"Erro: {e}")
-    return "Erro interno do servidor", 500
+    try:
+        return 'Hello, World!'
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro interno do servidor", 500
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
