fffe269e1073946a16b9504b09c2561b35ee1aa3
matheusRoboTip <matheus.carvalho@robotip.com.br>
2024-02-20 11:31:09 -0300

    test-5

--- api/main.py
+++ api/main.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello Vercel"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    try:
+        # Seu código aqui
+        return "Hello Vercel"
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro interno do servidor", 500
