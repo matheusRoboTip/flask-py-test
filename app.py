d266c2de7356d18ddd183df73871cd4d62f0e831
matheusRoboTip <matheus.carvalho@robotip.com.br>
2024-02-20 13:19:31 -0300

    text

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app)
 @app.route('/simulate')
 def hello_world():
     try:
-        return 'Hello, World!'
+        return 'Text return api OK'
     except Exception as e:
         print(f"Erro: {e}")
         return "Erro interno do servidor", 500
