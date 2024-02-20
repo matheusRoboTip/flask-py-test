1c6a62e683c1a487ab1dd8060c943163cae899a6
matheusRoboTip <matheus.carvalho@robotip.com.br>
2024-02-20 12:10:02 -0300

    test-7

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  try:
+    # Seu código aqui
+    return 'Hello, World!'
+  except Exception as e:
+    print(f"Erro: {e}")
+    return "Erro interno do servidor", 500
 
 if __name__ == '__main__':
   app.run(debug=True)
